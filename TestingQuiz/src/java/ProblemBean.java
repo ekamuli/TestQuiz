1038285a20d346c17cbe2c9ddf43c09265159eb4
EKA <EKA@Positron>
2014-11-19 09:05:04 +0700

    TestQuiz : Core JavaServer Faces Chapter 2

---
+++ TestingQuiz/src/java/ProblemBean.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author EKA
+ */
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ProblemBean implements Serializable {
+    private ArrayList<Integer> sequence;
+    private int solution;
+    
+    public ProblemBean(){}
+    
+    public ProblemBean(int[] values, int solution){
+        sequence = new ArrayList<Integer>();
+        for(int i = 0; i < values.length; i++){
+            sequence.add(values[i]);
+            this.solution = solution;
+        }
+    }
+
+    public ArrayList<Integer> getSequence() {
+        return sequence;
+    }
+    
+    public void setSequence(ArrayList<Integer> newValue) {
+        sequence = newValue;
+    }
+
+    public int getSolution() {
+        return solution;
+    }
+
+    public void setSolution(int newValue) {
+        solution = newValue;
+    }
+}
